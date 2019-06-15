589abb37575e702b8583e85cf87ca07fca87fb32
Gergely Bencsik <becsegergely@gmail.com>
2019-06-15 23:02:42 +0200

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+extern crate chrono;
+extern crate systemstat;
+
+use systemstat::{System, Platform};
+use chrono::Timelike;
+
+fn number_to_binary_str(num: u8) -> String
+{
+    let mut binary_str: String = "".to_string();
+    for bit in 0..8 
+    {
+        binary_str += match(num>>bit&0x01)
+        {
+            1 => "▀",
+            _ => "▄",
+        }
+    }
+    binary_str.chars().rev().collect::<String>()
+}
+
+fn main()
+{
+    let mut _status_text: String = "".to_string();
+    let sys = System::new();
+
+    match sys.battery_life()
+    {
+        Ok(battery) => _status_text=format!("{} BAT: {:02}%"
+                                            , _status_text
+                                            , (battery.remaining_capacity*100.0) as u8
+                                            ),
+        Err(e) => println!("{}", e),
+    }
+    let now = chrono::Local::now();
+    _status_text 
+        = format!(" {} | {}" 
+                  , _status_text
+                  , now.format("%Y-%m-%m %H:%M:%S"));
+
+    _status_text
+        = format!(" {} | {} {} {}"
+                  , _status_text
+                  , number_to_binary_str(now.time().hour() as u8)
+                  , number_to_binary_str(now.time().minute() as u8)
+                  , number_to_binary_str(now.time().second() as u8)
+                  );
+
+    println!("{}", _status_text);
+}
