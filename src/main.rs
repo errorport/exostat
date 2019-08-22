caa01d5f71170035c1135ac3287868307a3610bd
Gergely Bencsik <becsegergely@gmail.com>
2019-08-22 14:15:09 +0200

    Binary watch reversed.

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,27 @@ fn number_to_binary_str(num: u8) -> String
     let mut binary_str: String = "".to_string();
     for bit in 0..8 
     {
-        binary_str += match(num>>bit&0x01)
+        binary_str += match num>>bit&0x01
         {
-            //1 => "▀",
-            1 => "^d^●^aa0f00#c^",
+            1 => "^c#00f0aa^●^d^",
             _ => "●",
         }
     }
-    binary_str.chars().rev().collect::<String>()
+    binary_str
 }
 
 fn main()
 {
     let mut _status_text: String = "".to_string();
     let sys = System::new();
-
     match sys.battery_life()
     {
-        Ok(battery) => _status_text=format!("{} BAT: {:02}%"
-                                            , _status_text
-                                            , (battery.remaining_capacity*100.0) as u8
-                                            ),
+        Ok(battery) => 
+            _status_text=format!(
+            "{} BAT: {:02}%"
+            , _status_text
+            , (battery.remaining_capacity*100.0) as u8
+        ),
         Err(e) => println!("{}", e),
     }
     let now = chrono::Local::now();
@@ -41,9 +41,9 @@ fn main()
     _status_text
         = format!(" {} | {} {} {}"
                   , _status_text
-                  , number_to_binary_str(now.time().hour() as u8)
-                  , number_to_binary_str(now.time().minute() as u8)
                   , number_to_binary_str(now.time().second() as u8)
+                  , number_to_binary_str(now.time().minute() as u8)
+                  , number_to_binary_str(now.time().hour() as u8)
                   );
 
     println!("{}", _status_text);
